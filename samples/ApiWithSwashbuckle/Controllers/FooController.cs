e424a642d81d113493b733eb74fb61fc96e3d740
Mohammad Rahhal <m.r992@hotmail.com>
2021-12-09 17:52:47 +0200

    Create ApiWithSwashbuckle sample

---
+++ samples/ApiWithSwashbuckle/Controllers/FooController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MR.AspNetCore.Pagination;
+
+namespace ApiWithSwashbuckle.Controllers;
+
+public class User
+{
+	public int Id { get; set; }
+
+	public string Name { get; set; }
+}
+
+public class FooController
+{
+	[HttpGet("users1")]
+	public ActionResult<KeysetPaginationResult<User>> GetUsers1()
+	{
+		throw new NotImplementedException();
+	}
+
+	[HttpGet("users2")]
+	public Task<ActionResult<KeysetPaginationResult<User>>> GetUsers2()
+	{
+		throw new NotImplementedException();
+	}
+
+	[HttpGet("users3")]
+	public ActionResult<OffsetPaginationResult<User>> GetUsers3()
+	{
+		throw new NotImplementedException();
+	}
+
+	[HttpGet("users4")]
+	public Task<ActionResult<OffsetPaginationResult<User>>> GetUsers4()
+	{
+		throw new NotImplementedException();
+	}
+}
---
+++ samples/ApiWithSwashbuckle/Program.cs
@@ -0,0 +1,48 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddMvc();
+
+builder.Services.AddPagination(c =>
+{
+	c.PageQueryParameterName = "p";
+});
+
+builder.Services.AddSwaggerGen(c =>
+{
+	c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
+	c.CustomSchemaIds(x => x.FullName);
+
+	c.TagActionsBy(api => new[] { api.RelativePath });
+
+	c.AddPaginationOperationFilter();
+});
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Error");
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
+{
+	c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api");
+	c.OAuthClientId("swagger");
+});
+
+app.UseEndpoints(endpoints =>
+{
+	endpoints.MapControllers();
+});
+
+app.Run();
