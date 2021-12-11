24c5326e39d56cb7bf4d0f96cc96f17b018f4755
Mohammad Rahhal <m.r992@hotmail.com>
2021-12-11 20:09:22 +0200

    Comments

--- src/MR.AspNetCore.Pagination/PaginationResult.cs
+++ src/MR.AspNetCore.Pagination/PaginationResult.cs
@@ -6,9 +6,9 @@
 /// <typeparam name="T">The type of the data list item.</typeparam>
 /// <param name="Data">The data list.</param>
 /// <param name="TotalCount">The total count of the data.</param>
+/// <param name="PageSize">The size of the page. This can be different from the actual size of <see cref="Data"/>.</param>
 /// <param name="HasPrevious">Whether there's previous data to the list.</param>
 /// <param name="HasNext">Whether there's next data to the list.</param>
-/// <param name="PageSize">The size of the page. This can be different from the actual size of <see cref="Data"/>.</param>
 public record KeysetPaginationResult<T>(
 	IEnumerable<T> Data,
 	int TotalCount,
